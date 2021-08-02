90f3b67ccc951218f438d5ac6d3610d202429b8a
201602569 <christophersoto97@gmail.com>
2021-08-01 18:23:21 -0600

    caliicacion sopes

--- servernode/index.js
+++ servernode/index.js
@@ -4,12 +4,6 @@ const BodyParser = require("body-parser");
 
 
 
-const redis = require('redis');
-const client = redis.createClient();
-
-
-
-const client = redis.createClient(port, host);
 var app = Express();
 
 Mongoose.connect('mongodb+srv://chris:amor4219@cluster0.3plrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
